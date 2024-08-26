e86e38a6f4458ba06694f14dbb8d80fabb0e0a0a
iTrue <109562378+carlosyamanaka@users.noreply.github.com>
2024-08-26 10:28:35 -0300

    Projeto Expenses inicializado

---
+++ expenses/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas pessoais'),
+      ),
+      body: Center(
+        child: Text('Versão inicial'),
+      ),
+    );
+  }
+}
